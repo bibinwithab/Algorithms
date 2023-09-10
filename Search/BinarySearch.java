3ce45d2062efb8f75ccd7660f5ec7a5930b6ffc8
bibinwithab <bibingraceson@gmail.com>
2023-09-10 16:36:41 +0530

    Linear Search

--- Search/BinarySearch.java
+++ Search/BinarySearch.java
@@ -1,5 +1,24 @@
-package Search;
+
 
 public class BinarySearch {
-    
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4 };
+        int key = 3;
+        int index = binarySearch(arr, key);
+        System.out.println(index);
+    }
+
+    public static int binarySearch(int[] arr, int key) {
+        int low = 0, high = arr.length - 1, mid;
+        while (low <= high) {
+            mid = (low + high) / 2;
+            if (arr[mid] == key)
+                return mid;
+            else if (arr[mid] > key)
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        return -1;
+    }
 }
--- Search/LinearSearch.java
+++ Search/LinearSearch.java
@@ -1,5 +1,27 @@
-package Search;
+// The LinearSearch also called Sequential Search is one of the brute force methods to find an element in an array.
 
-public class LinearSearch{
-    
+// Time Complexity: O(n); Where n is the size off the array. So, It is not very efficient fot large arrays
+
+// Space Complexity: O(1); Space allocation does not depend on the size of the array. It is always constant.
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5 };
+        int key = 3;
+        int index = linearSearch(arr, key);
+        if (index == -1) {
+            System.out.println("Element is not found");
+        }else {
+            System.out.println("Key element is found at index: "+index);
+        }
+    }
+
+    // The given is checked through-out the array and the index is returned if found.
+
+    public static int linearSearch(int[] arr, int key) {
+        for (int i = 0; i < arr.length; i++)
+            if (arr[i] == key)
+                return i;
+        return -1;
+    }
 }
