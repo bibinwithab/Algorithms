bb79be6b07f4666512238b40a74091a297133098
bibinwithab <bibingraceson@gmail.com>
2023-09-10 23:23:21 +0530

    Merge Sort

--- Sort/MergeSort.java
+++ Sort/MergeSort.java
@@ -1,3 +1,80 @@
+// In MergeSort, the original array  is divided  into sub arrays and each sub arrays are sorted and merged
+
+// Time Complexity: O(nlogn); uses divide and conquer approach. Useful to sort large datasets.
+
+// Space Complexity: O(n); uses extra space. 'n' is the number of sub arrays.
+
 public class MergeSort {
-    
+    public static void main(String[] args){
+        int[] arr = {5, 4, 3, 2, 1};
+        mergeSort(arr, 0, arr.length-1);
+        for(int i : arr){
+            System.out.print(i + " ");
+        }
+    }
+
+    private static void mergeSort(int[] arr, int l, int r) {
+        if (l < r) {
+            // Find the middle point
+            int m = (l+r)/2;
+
+            // Sort first and second halves
+            mergeSort(arr, l, m);
+            mergeSort(arr, m+1, r);
+
+            // Merge the sorted halves
+            merge(arr, l, m, r);
+        }
+    }
+
+    private static void merge(int[] arr, int l, int m, int r) {
+        // Find sizes of two subarrays to be merged
+        int n1 = m-l+1;
+        int n2 = r-m;
+
+        // Create temp arrays
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        // Copy data to temp arrays
+        for (int i = 0; i < n1; i++) {
+            L[i] = arr[l+i];
+        }
+        for (int j = 0; j < n2; j++) {
+            R[j] = arr[m+1+j];
+        }
+
+        // Merge the temp arrays
+
+        // Initial indexes of first and second subarrays
+        int i = 0, j = 0;
+
+        // Initial index of merged subarray
+        int k = l;
+
+        while (i<n1 && j<n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++; 
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Copy remaining elements of L[] if any
+        while (i<n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        // Copy remaining elements of R[] if any
+        while (j<n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
 }
